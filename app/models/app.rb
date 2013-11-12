4472e39626a43c3e0e5c003e7d8e3c0b19780d4c
montekaka <josh.chen.jia@gmail.com>
2013-11-11 23:30:24 -0800

    Add logic to handle mid-night issue of Mixpanel API

--- app/models/app.rb
+++ app/models/app.rb
@@ -21,15 +21,60 @@ class App < ActiveRecord::Base
 
 		puts "************ Mixpanel loaded!!!! ****************"
 		puts data.inspect
-
+		result = 0
 		if data["legend_size"] == 0
 			return 0
-		else
+		elsif data["data"]["series"].length == 1						
 			data["data"]["values"].each do |key, value|
 				value.each do |k, v|
 					return v
 				end
 			end
+		else
+			data["data"]["values"].each do |key, value|				
+				value.each do |k, v|
+					result = result + v
+				end
+			end
+			return result
+		end
+	end
+
+	def get_itunes_button_click_count
+		# get page_count from mixpanel
+		config = {
+			api_key: 'dc026b4b9413dcce8e3ac0374ef906e5', 
+			api_secret: 'd144302ca4443c49420569d07d1c9e15'
+		}
+
+		client = Mixpanel::Client.new(config)
+		
+		data = client.request('events/properties', {
+		  event:     'Click Apps iTunes',  
+		  name:      'Landing Page ID',
+		  values:     [self.id.to_s],
+		  type:      'unique',
+		  unit:      'day',
+		  interval:   1,
+		})
+		
+		puts "************ Mixpanel loaded button Click!!!! ****************"
+		result = 0
+		if data["legend_size"] == 0
+			return 0
+		elsif data["data"]["series"].length == 1						
+			data["data"]["values"].each do |key, value|
+				value.each do |k, v|
+					return v
+				end
+			end
+		else
+			data["data"]["values"].each do |key, value|				
+				value.each do |k, v|
+					result = result + v
+				end
+			end
+			return result
 		end
 	end
 end
